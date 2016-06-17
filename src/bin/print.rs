b4b192cf9fbcfb674cdf7071c6c5183ff82eaa24
Alexey Arbuzov <lexx.vir@gmail.com>
2016-06-17 21:04:57 +0300

    Print chained tlvs.

--- src/bin/print.rs
+++ src/bin/print.rs
@@ -6,7 +6,7 @@ use tlv_parser::*;
 
 use rustc_serialize::hex::FromHex;
 
-fn print_tag( tag: Vec<u8> ) {
+fn print_tag( tag: &Vec<u8> ) {
     print!( "tag=" );
     for x in tag {
         print!("{:02X}", x);
@@ -14,17 +14,17 @@ fn print_tag( tag: Vec<u8> ) {
     println!("");
 }
 
-fn print( tlv: Tlv, ident: usize ) {
+fn print( tlv: &Tlv, ident: usize ) {
     for _ in 0..ident {
         print!(" ");
     }
 
     match tlv.val {
-        Value::TlvList( list ) => {
-            print_tag( tlv.tag );
+        Value::TlvList( ref list ) => {
+            print_tag( &tlv.tag );
 
             for t in list {
-                print(t, ident + 4);
+                print(&t, ident + 4);
             }
         },
         Value::Val( _ ) => {
@@ -38,8 +38,19 @@ fn main() {
 	let mut input = String::new();
 	std::io::stdin().read_to_string( &mut input ).unwrap();
 
-    match Tlv::from_vec(&input.from_hex().unwrap()) {
-        Ok(tlv) => print( tlv, 0 ),
-        Err(err) => println!("Error: {}", err),
+    let buf = input.from_hex().unwrap();
+    let mut idx = 0;
+
+    while idx < buf.len() {
+        match Tlv::from_vec(&buf[idx..]) {
+            Ok(tlv) => {
+                print( &tlv, 0 );
+                idx += tlv.len();
+            },
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
     }
 }
