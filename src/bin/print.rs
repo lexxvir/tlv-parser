34648b75cc0a645b143bd7b34f2eddf3309d88b3
Alexey Arbuzov <lexx.vir@gmail.com>
2016-06-17 18:54:26 +0300

    Print nested tags with identation.

--- src/bin/print.rs
+++ src/bin/print.rs
@@ -6,13 +6,40 @@ use tlv_parser::*;
 
 use rustc_serialize::hex::FromHex;
 
+fn print_tag( tag: Vec<u8> ) {
+    print!( "tag=" );
+    for x in tag {
+        print!("{:02X}", x);
+    }
+    println!("");
+}
+
+fn print( tlv: Tlv, ident: usize ) {
+    for _ in 0..ident {
+        print!(" ");
+    }
+
+    match tlv.val {
+        Value::TlvList( list ) => {
+            print_tag( tlv.tag );
+
+            for t in list {
+                print(t, ident + 4);
+            }
+        },
+        Value::Val( _ ) => {
+            println!("{}", tlv);
+        },
+        _ => (),
+    }
+}
+
 fn main() {
 	let mut input = String::new();
 	std::io::stdin().read_to_string( &mut input ).unwrap();
 
-	match Tlv::from_vec(&input.from_hex().unwrap()) {
-        Ok(tlv) => println!("{:}", tlv),
+    match Tlv::from_vec(&input.from_hex().unwrap()) {
+        Ok(tlv) => print( tlv, 0 ),
         Err(err) => println!("Error: {}", err),
     }
-
 }
