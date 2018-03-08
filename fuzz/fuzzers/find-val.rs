bd2ddc6daecc65f14b033231ac01949b09aa38d2
Alexey Arbuzov <lexx.vir@gmail.com>
2018-03-08 15:17:13 +0300

    Update fuzzer's code

--- fuzz/fuzzers/find-val.rs
+++ fuzz/fuzzers/find-val.rs
@@ -1,11 +1,12 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate tlv_parser;
 
 use tlv_parser::tlv::*;
 
 fuzz_target!(|data: &[u8]| {
-    let tlv = Tlv::new(0x6a, Value::Nothing);
+    let tlv = Tlv::new(0x6a, Value::Nothing).unwrap();
     let s = String::from_utf8_lossy(data);
     let _ = tlv.find_val(&s);
 });
